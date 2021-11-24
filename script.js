96ea54edf91cc69a58a9e57d6cf1393c1e90d5ce
xiaoxuanw <54602678+xiaoxuanw@users.noreply.github.com>
2021-11-24 10:14:06 -0500

    commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//cursor animation
 var cursor = $(".cursor");
 
 var mouseX = 0,
@@ -28,3 +29,39 @@ $(".clickable-item").on("mouseenter", function() {
 $(".clickable-item").on("mouseleave", function() {
     cursor.removeClass("active")
 });
+
+// horizontal scroll
+const frame = document.querySelector('.frame')
+
+// window.addEventListener('scroll', () => {
+//     var scrolled = window.pageYOffset;
+//     console.log(scrolled)
+
+//     const val = scrolled * 0.6;
+//     frame.style.transform = `translateX(${0.45*val}px)`
+// })
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        // If the element is visible
+        if (entry.isIntersecting) {
+            // Add the animation class
+            TweenMax.to('.frame', 1, { x: -600 })
+
+            // frame.style.transform = "translateX(-500px)";
+
+
+            frame.classList.add('frame-animation');
+        }
+        // frame.classList.remove('frame-animation');
+
+    });
+});
+observer.observe(document.querySelector('.track'));
+TweenMax.to('.frame', 1, { x: -600 })
+
+var scene = new ScrollMagic.Scene({ triggerElement: ".track", duration: 200, offset: -50 })
+    .setTween(tween)
+    .addIndicators({ name: "tween css class" }) // add indicators (requires plugin)
+    .addTo(controller);
